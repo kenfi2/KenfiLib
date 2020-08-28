e55e4e4ff87052669460de3f683168be460d2eee
João Vítor Matias Santana <57517248+kenfi2@users.noreply.github.com>
2020-08-28 03:49:25 -0300

    Fix creature functions

--- monster.lua
+++ monster.lua
@@ -5,14 +5,14 @@ MonsterLib = {
 }
 
 function Monster(uid)
-	if tonumber(uid) then
-		return setmetatable({id = uid}, {__index = setmetatable(MonsterLib, {__index = Creature(uid)})})
+	if tonumber(uid) and isMonster(uid) then
+		return setmetatable({id = uid}, {__index = setmetatable(MonsterLib, {__index = CreatureLib}), __eq = eq_event(a, b)})
 	elseif getmetatable(uid) and Creature(uid):isMonster() then
 		uid = Creature(uid)
-		return setmetatable({id = uid:getId()}, {__index = setmetatable(MonsterLib, {__index = uid})})
+		return setmetatable({id = uid:getId()}, {__index = setmetatable(MonsterLib, {__index = CreatureLib}), __eq = eq_event(a, b)})
 	elseif getCreatureByName(uid) then
 		if isMonster(getCreatureByName(uid)) then
-			return setmetatable({id = getCreatureByName(uid)}, {__index = setmetatable(MonsterLib, {__index = Creature(getCreatureByName(uid))})})
+			return setmetatable({id = getCreatureByName(uid)}, {__index = setmetatable(MonsterLib, {__index = CreatureLib}), __eq = eq_event(a, b)})
 		end
 	end
 end
