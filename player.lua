37730499ca158ea92416685b7a9b456eab932d77
João Vítor Matias Santana <57517248+kenfi2@users.noreply.github.com>
2020-08-28 03:50:00 -0300

    Update player.lua

--- player.lua
+++ player.lua
@@ -33,14 +33,59 @@ PlayerLib = {
 	end,
 	getMana = function(self) return getCreatureMana(self.id) end,
 	getMaxMana = function(self) return getCreatureMaxMana(self.id) end,
+	getLastLoginSaved = function(self)
+		return getPlayerLastLogin(self.id)
+	end,
+	getAccountManager = function(self)
+		return getPlayerAccountManager(self.id)
+	end,
+	getFreeCapacity = function(self)
+		return getPlayerFreeCap(self.id)
+	end,
+	getDepotItems = function(self, depotId)
+		return getPlayerDepotItems(self.id, depotId)
+	end,
+	getSkullTime = function(self)
+		return getPlayerSkullEnd(self.id)
+	end,
+	setSkullTime = function(self, skullTime, skullType)
+		return doPlayerSetSkullEnd(self.id, skullTime, skullType or self:getSkull())
+	end,
+	getExperience = function(self)
+		return getPlayerExperience(self.id)
+	end,
+	addExperience = function(self, amount)
+		doPlayerAddExperience(cid, math.abs(amount))
+	end,
+	removeExperience = function(self, amount)
+		doPlayerAddExperience(cid, -(math.abs(amount)))
+	end,
+	getMagicLevel = function(self)
+		return getPlayerMagLevel(self.id)
+	end,
+	getBaseMagicLevel = function(self)
+		return getPlayerMagLevel(self.id, true)
+	end,
+	getManaSpent = function(self)
+		return getPlayerSpentMana(self.id)
+	end,
+	addManaSpent = function(self, amount)
+		doPlayerAddSpentMana(self.id, amount, true)
+	end,
+	getBaseMaxHealth = function(self)
+		return getCreatureMaxHealth(self.id, true)
+	end,
+	getBaseMaxMana = function(self)
+		return getCreatureMaxMana(self.id, true)
+	end,
 }
 
 function Player(uid)
 	if tonumber(uid) and isPlayer(uid) then
-		return setmetatable({id = uid}, {__index = setmetatable(PlayerLib, {__index = CreatureLib}), __eq = eq_event()})
+		return setmetatable({id = uid}, {__index = setmetatable(PlayerLib, {__index = CreatureLib}), __eq = eq_event(a, b)})
 	elseif getmetatable(uid) then
-		return setmetatable({id = uid:getId()}, {__index = setmetatable(PlayerLib, {__index = CreatureLib}), __eq = eq_event()})
+		return setmetatable({id = uid:getId()}, {__index = setmetatable(PlayerLib, {__index = CreatureLib}), __eq = eq_event(a, b)})
 	elseif getPlayerByName(uid) then
-		return setmetatable({id = getPlayerByName(uid)}, {__index = setmetatable(PlayerLib, {__index = CreatureLib}), __eq = eq_event()})
+		return setmetatable({id = getPlayerByName(uid)}, {__index = setmetatable(PlayerLib, {__index = CreatureLib}), __eq = eq_event(a, b)})
 	end
 end
