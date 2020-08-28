5a9200cb45e5b34a2a4689697ab6cd19f34042ab
João Vítor Matias Santana <57517248+kenfi2@users.noreply.github.com>
2020-08-28 03:54:31 -0300

    Update town.lua

--- town.lua
+++ town.lua
@@ -6,8 +6,8 @@ TownLib = {
 
 function Town(uid)
 	if tonumber(uid) then
-		return setmetatable({id = uid}, {__index = TownLib})
+		return setmetatable({id = uid}, {__index = TownLib, __eq = eq_event(a, b),})
 	elseif getmetatable(uid) then
-		return setmetatable({id = uid:getId()}, {__index = TownLib})
+		return setmetatable({id = uid:getId()}, {__index = TownLib, __eq = eq_event(a, b),})
 	end
 end
