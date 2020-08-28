29d503483f120ced5e7fdc208315bfb9c87accf3
João Vítor Matias Santana <57517248+kenfi2@users.noreply.github.com>
2020-08-28 00:22:26 -0300

    Update position.lua

--- position.lua
+++ position.lua
@@ -11,9 +11,53 @@ function Position(x, y, z, stackpos)
 	if type(x) == "table" then
 		pos = x
 		if pos.x and pos.y and pos.z then
-			return setmetatable(pos, {__index = PositionLib})
+			return setmetatable(pos, {
+			__index = PositionLib,
+			__add = function(pos, value)
+				if type(value) == "table" or getmetatable(value) then
+					if value.x and value.y and value.z then
+						return Position(pos.x+value.x,pos.y+value.x,pos.z+value.x)
+					end
+				elseif tonumber(value) then
+					return Position(pos.x+value,pos.y+value,pos.z+value)
+				end
+				return false
+			end,
+			__sub = function(pos, value)
+				if type(value) == "table" or getmetatable(value) then
+					if value.x and value.y and value.z then
+						return Position(pos.x-value.x,pos.y-value.y,pos.z-value.z)
+					end
+				elseif tonumber(value) then
+					return Position(pos.x-value,pos.y-value,pos.z-value)
+				end
+				return false
+			end,
+		})
 		end
 	else
-		return setmetatable({x=x,y=y,z=z,stackpos=stackpos or 0}, {__index = PositionLib})
+		return setmetatable({x=x,y=y,z=z,stackpos=stackpos or 0}, {
+		__index = PositionLib,
+		__add = function(pos, value)
+			if type(value) == "table" or getmetatable(value) then
+				if value.x and value.y and value.z then
+					return Position(pos.x+value.x,pos.y+value.x,pos.z+value.x)
+				end
+			elseif tonumber(value) then
+				return Position(pos.x+value,pos.y+value,pos.z+value)
+			end
+			return false
+		end,
+		__sub = function(pos, value)
+			if type(value) == "table" or getmetatable(value) then
+				if value.x and value.y and value.z then
+					return Position(pos.x-value.x,pos.y-value.y,pos.z-value.z)
+				end
+			elseif tonumber(value) then
+				return Position(pos.x-value,pos.y-value,pos.z-value)
+			end
+			return false
+		end,
+	})
 	end
 end
